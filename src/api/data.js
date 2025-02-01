fe958ac09607dcfa68daef5573459fe86505a0de
Todor Belchev <todorbelchev@live.com>
2025-02-01 16:11:49 +0200

    Added deploy to render.com

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
+import host from '../api/host.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+api.settings.host = host;
 
 export const login = api.login;
 export const register = api.register;
@@ -12,38 +12,38 @@ export const logout = api.logout;
 
 
 export async function createBook(data) {
-    return await api.post(host + '/data/books', data);
+    return await api.post(api.settings.host + '/data/books', data);
 }
 
 
 export async function getAllBooks() {
-    return await api.get(host + '/data/books?sortBy=_createdOn%20desc');
+    return await api.get(api.settings.host + '/data/books?sortBy=_createdOn%20desc');
 }
 
 export async function getBookById(id) {
-    return await api.get(host + '/data/books/' + id);
+    return await api.get(api.settings.host + '/data/books/' + id);
 }
 
 export async function deleteBookById(id) {
-    return await api.del(host + '/data/books/' + id);
+    return await api.del(api.settings.host + '/data/books/' + id);
 }
 
 export async function updateBook(id, data) {
-    return await api.put(host + '/data/books/' + id, data);
+    return await api.put(api.settings.host + '/data/books/' + id, data);
 }
 
 export async function getUserBooks(userId) {
-    return await api.get(host + `/data/books?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
+    return await api.get(api.settings.host + `/data/books?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
 }
 
 export async function getBookTotalLikes(bookId) {
-    return await api.get(host + `/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
+    return await api.get(api.settings.host + `/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
 }
 
 export async function likeBookApi(bookId) {
-    return await api.post(host + `/data/likes`, { bookId });
+    return await api.post(api.settings.host + `/data/likes`, { bookId });
 }
 
 export async function isUserAlreadyLiked(bookId, userId) {
-    return await api.get(host + `/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
+    return await api.get(api.settings.host + `/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
 }
---
+++ src/api/host.js
@@ -0,0 +1,1 @@
+export default 'https://librarycatalogsoftuni.onrender.com/';
